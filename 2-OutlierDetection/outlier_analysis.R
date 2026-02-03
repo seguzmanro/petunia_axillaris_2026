7d2820322640dcb262b0b9b160e2411803af3d1c
seguzmanro <54817021+seguzmanro@users.noreply.github.com>
2026-02-03 12:54:30 -0300

    updated final outlier script

--- 2-OutlierDetection/outlier_analysis.R
+++ 2-OutlierDetection/outlier_analysis.R
@@ -1,1 +1,19 @@
-library('vcfR')
+library(vcfR)
+library(dplyr)
+
+setwd('2-OutlierDetection/')
+
+vcf_noLD <- read.vcfR('../1-VariantCallFilt/07_freebayes/Paxil_M095_noLD.recode.vcf.gz')
+
+pcadapt_outliers <- read.table('PCAdapt/Paxil_M095_noLD_Outliers_PCAdapt.txt')
+colnames(pcadapt_outliers) <- c('CHROM', 'POS')
+bayescan_outliers <- read.table('Bayescan/Paxil_M095_noLD_Bayescan_Outliers_BA_01.txt')
+colnames(bayescan_outliers) <- c('CHROM', 'POS')
+
+common <- pcadapt_outliers[apply(pcadapt_outliers, 1, paste, collapse = "|") %in%
+              apply(bayescan_outliers, 1, paste, collapse = "|"), ]
+
+print(common)
+
+total_outliers <- rbind(pcadapt_outliers, bayescan_outliers)
+write.table(total_outliers, 'Paxil_M095_noLD_TotalOutliers.txt', sep='\t', col.names=F, row.names=F, quote=F)
